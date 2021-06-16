d6839a6b50a031184676d874e2bba12f31ed3e87
Serg Lotz <blondewalker@gmail.com>
2021-06-16 10:00:40 +0200

    Add about text

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import {
+  Avatar,
+  Box,
+  Container,
+  Card,
+  CardHeader,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -22,8 +35,19 @@ class About extends React.Component {
     const classes = useStyles();
 
     return (
-      <Container>
-        About
+      <Container fixed>
+        <Typography variant="h1" component="h1" gutterBottom>
+          About
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          munichNFT is the world's first city-NFT and a platform for creatives and artists who want to learn about, create, mint, auction and buy NFTs. Become part of our MunichNFT community in the Web 3.0 revolution by joining us on discord and twitter!
+        </Typography>
+        <Typography variant="h2" component="h2" gutterBottom>
+          Core Team
+        </Typography>
+        <Typography variant="body1" gutterBottom>
+          munichNFT was initiaed by Sergej, Yonne, Isabell, Christian and Nick
+        </Typography>
       </Container>
     );
   }
