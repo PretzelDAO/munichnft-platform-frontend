7445fefa4f5cf23c0928fec488abf163cce94d8b
Nick Stracke <nick.stracke@web.de>
2021-06-16 12:42:35 +0200

    implement artist and about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import Profile from '../Components/Profile';
+
 import {
   Avatar,
   Box,
@@ -13,19 +15,92 @@ import {
   Typography,
   Grid,
   Divider,
+  Link
 } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
-  root: {
+import nick from '../res/nick.jpg';
+import isabell from '../res/isabell.jpg';
+import yonne from '../res/yonne.jpg';
+import lukas from '../res/lukas.jpg';
+import sergej from '../res/sergej.jpg';
+import christian from '../res/christian.jpg';
+import felix from '../res/felix.jpg';
+import johannes from '../res/johannes.jpg';
 
-  },
+const useStyles = makeStyles(theme => ({
+  root: {}
 }));
 
 class About extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      core: [{
+        name: 'Prof. Dr. Isabell M. Welpe',
+        image: isabell,
+        description: 'Professor for Strategy & Organisation @TUM',
+        title: 'Head of Strategy',
+        twitter: 'https://twitter.com/IsabellWelpe',
+        linkedin: 'https://www.linkedin.com/in/isabell-welpe-83a202/'
+      },
+      {
+        name: 'Yonne-Luca Hack',
+        image: yonne,
+        description: 'M.A. Architecture @TUM, Urban Enthusiast',
+        title: 'Creative Director',
+        twitter: 'https://twitter.com/HackYonne',
+        linkedin: 'https://www.linkedin.com/in/yonne-luca-hack-86286bb1/'
+      },
+      {
+        name: 'Sergej Lotz',
+        image: sergej,
+        description: 'CDTM, M.Sc. Technology Management @LMU',
+        title: 'Head of Product',
+        twitter: 'https://twitter.com/serglotz',
+        linkedin: 'https://www.linkedin.com/in/serglotz/'
+      },
+      {
+        name: 'Christian Ziegler',
+        image: christian,
+        description: 'CTO Blockcurators GmbH, Graduate CS Student @TUM',
+        title: 'Tech Lead',
+        twitter: 'https://twitter.com/Totenfluch',
+        linkedin: 'https://www.linkedin.com/in/christian-ziegler-26a45a132/'
+      },
+      {
+        name: 'Nick Stracke',
+        image: nick,
+        description: 'CDTM, M.Sc. Data Science @LMU',
+        title: 'Blockchain Developer',
+        twitter: 'https://twitter.com/StrackeNick',
+        linkedin: 'https://www.linkedin.com/in/nick-stracke-327100141/'
+      },
+      ],
+      helpers: [{
+        name: 'Lukas Ruppert',
+        image: lukas,
+        description: 'Management & Technology @ TUM',
+        title: 'Coordinator',
+        twitter: 'https://twitter.com/Lk_Ruppert',
+        linkedin: 'https://www.linkedin.com/in/lukas-ruppert/'
+      },
+      {
+        name: 'Felix von Stumpfeldt',
+        image: felix,
+        description: 'Politics & Technology @TUM',
+        title: 'Social Media',
+        twitter: 'https://twitter.com/Felixvomfeldt',
+        linkedin: 'https://www.linkedin.com/in/felix-von-stumpfeldt-aa6848170/'
+      },
+      {
+        name: 'Johannes Theisen',
+        image: johannes,
+        description: 'Information Systems @TUM',
+        title: 'Developer',
+        twitter: 'https://twitter.com/',
+        linkedin: 'https://www.linkedin.com/in/johannes-theisen-b8111b149/'
+      },
+    ],
     };
 
     this.RenderAbout = this.RenderAbout.bind(this);
@@ -42,12 +117,37 @@ class About extends React.Component {
         <Typography variant="body1" gutterBottom>
           munichNFT is the world's first city-NFT and a platform for creatives and artists who want to learn about, create, mint, auction and buy NFTs. Become part of our MunichNFT community in the Web 3.0 revolution by joining us on discord and twitter!
         </Typography>
+
         <Typography variant="h2" component="h2" gutterBottom>
           Core Team
         </Typography>
-        <Typography variant="body1" gutterBottom>
-          munichNFT was initiaed by Sergej, Yonne, Isabell, Christian and Nick
+        <Grid container spacing={6} alignContent="center" justify="flex-start" alignItems="flex-start">
+          {
+            this.state.core.map((p) => {
+              return (
+                <Grid item xs={12} sm={6} md={4}>
+                  <Profile person={p}/>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+
+
+        <Typography variant="h2" component="h2" gutterBottom style={{marginTop: '48px'}}>
+          Helpers
         </Typography>
+        <Grid container spacing={6} alignContent="center" justify="flex-start" alignItems="flex-start">
+          {
+            this.state.helpers.map((p) => {
+              return (
+                <Grid item xs={12} sm={6} md={4}>
+                  <Profile person={p}/>
+                </Grid>
+              )
+            })
+          }
+        </Grid>
       </Container>
     );
   }
--- src/Pages/Artists.jsx
+++ src/Pages/Artists.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import Profile from '../Components/Profile';
+
+import {
+  Avatar,
+  Box,
+  Container,
+  Card,
+  CardHeader,
+  CardActionArea,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Typography,
+  Grid,
+  Divider,
+  Link
+} from "@material-ui/core";
+
+import domi from '../res/domi.jpg';
+import max from '../res/max.jpg';
+import tomal from '../res/tomal.png';
+import nils from '../res/nils.jpg';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -12,7 +33,35 @@ class Artists extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      artists: [{
+        name: 'Dominik Bais',
+        image: domi,
+        description: `"As a visual artist, filmmaker and founder of the „OpenSource Archive“, Dominik Bais deals with concepts of the digital in relation to matter, memory, storage of information and its social impact."`,
+        twitter: 'https://dominikbais.com/',
+        instagram: 'https://www.instagram.com/dominikbais/'
+      },
+      {
+        name: 'Max Haarich',
+        image: max,
+        description: `"I am a Munich-based conceptual artist and ethics consultant with focus on technology. Munich is the place where a lot of money and tech is concentrated at the moment. In my opinion, arts can help to reflect the usage and impact of those resources. Therefore, my favorite place is the Kreativquartier where more and more Start-up's settle amid a lively arts scene. I am at the same time critical and enthusiastic about NFT and have given numerous interviews about my NFT art, which which integrates both perspectives. I would like to represent Munich with a balanced and responsible view on this fascinating technology."`,
+        twitter: 'https://twitter.com/UzupisMUC',
+        instagram: 'https://www.instagram.com/uzupis_munich/'
+      },
+      {
+        name: 'Nils Fröhling',
+        image: nils,
+        description: `"Munich based architect & digital artist."`,
+        twitter: ' https://twitter.com/FroehlingNils',
+        instagram: 'https://www.instagram.com/nils.froehling/'
+      },
+      {
+        name: 'Tomal K. Ganguly (tkg/nft)',
+        image: tomal,
+        description: `"Munich-based blockchain pro combining business, art and culture."`,
+        twitter: 'https://twitter.com/tomalganguly',
+        instagram: 'https://www.instagram.com/tomstr83'
+      },
+      ],
     };
 
     this.RenderArtists = this.RenderArtists.bind(this);
@@ -22,8 +71,21 @@ class Artists extends React.Component {
     const classes = useStyles();
 
     return (
-      <Container>
-        Artists
+      <Container fixed>
+        <Typography variant="h2" component="h2" gutterBottom>
+          Artists
+        </Typography>
+        <Grid container spacing={6} alignContent="center" justify="flex-start" alignItems="flex-start">
+          {
+            this.state.artists.map((p) => {
+              return (
+                <Grid item xs={12} sm={6} md={3}>
+                  <Profile person={p} />
+                </Grid>
+              )
+            })
+          }
+        </Grid>
       </Container>
     );
   }
